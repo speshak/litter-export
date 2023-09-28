f0e7521cf67d5022e9c3fb2cf2ead7e0ac5a27dd
Scott Peshak <scott@peshak.net>
2023-09-27 20:45:00 -0500

    Lint fixup

--- litterbot-export.py
+++ litterbot-export.py
@@ -1,4 +1,4 @@
-from prometheus_client import start_http_server, Summary, Gauge, Enum, Info
+from prometheus_client import start_http_server, Gauge, Enum, Info
 from pylitterbot import Account
 import asyncio
 import logging
@@ -23,35 +23,39 @@ async def main():
     # Define metrics.
     robot_info = Info('litterbot_info', 'Robot info', ['serial'])
     online_enum = Enum('litterbot_online_state', 'Robot online state',
-        ['serial'], states=['online', 'offline'])
-    litter_level = Gauge('litterbot_litter_level', 'Level of litter', ['serial'])
+                       ['serial'], states=['online', 'offline'])
+    litter_level = Gauge('litterbot_litter_level', 'Level of litter',
+                         ['serial'])
     waste_level = Gauge('litterbot_waste_level', 'Level of waste', ['serial'])
-    cycle_count = Gauge('litterbot_cycle_count', 'Number of cycles since last reset', ['serial'])
+    cycle_count = Gauge('litterbot_cycle_count',
+                        'Number of cycles since last reset', ['serial'])
 
     while True:
         try:
             # Connect to the API and load robots.
             logger.info("Connecting to the API")
-            await account.connect(username=username, password=password, load_robots=True)
+            await account.connect(
+                username=username,
+                password=password,
+                load_robots=True)
 
             # Print robots associated with account.
             logger.info(f"Found {len(account.robots)} robots")
             for robot in account.robots:
-                logger.info(f"Gather info for robot '{robot.name}' ({robot.serial})")
+                logger.info(f"Gather info for robot '{robot.name}' "
+                            f"({robot.serial})")
                 robot_info.labels(robot.serial).info({
                     'name': robot.name,
                     'model': robot.model,
                 })
 
                 # Update the metrics values
-                online_enum.labels(robot.serial).state('online' if robot.is_online else 'offline')
+                online_enum.labels(robot.serial).state(
+                    'online' if robot.is_online else 'offline')
 
                 waste_level.labels(robot.serial).set(robot.waste_drawer_level)
                 litter_level.labels(robot.serial).set(robot.litter_level)
                 cycle_count.labels(robot.serial).set(robot.cycle_count)
-
-                #print(await robot.get_insight())
-                #print(await robot.get_activity_history())
 
         finally:
             time.sleep(600)
