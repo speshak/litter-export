156bf7dd7c17b5d4b004b30518cf0408c4a0db2e
Scott Peshak <scott@peshak.net>
2023-09-28 20:16:02 -0500

    Add more metrics
    And remove the system generated ones.

--- litterbot-export.py
+++ litterbot-export.py
@@ -1,4 +1,5 @@
-from prometheus_client import start_http_server, Gauge, Enum, Info
+import prometheus_client
+from prometheus_client import start_http_server, Gauge, Enum, Info, Histogram, REGISTRY
 from pylitterbot import Account
 import asyncio
 import logging
@@ -12,6 +13,11 @@ logger = logging.getLogger("litter-exporter")
 # Set email and password for initial authentication.
 username = os.environ.get('LITTERBOT_USERNAME')
 password = os.environ.get('LITTERBOT_PASSWORD')
+
+# Disable the default collectors
+prometheus_client.REGISTRY.unregister(prometheus_client.GC_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PLATFORM_COLLECTOR)
+prometheus_client.REGISTRY.unregister(prometheus_client.PROCESS_COLLECTOR)
 
 
 async def main():
@@ -29,6 +35,11 @@ async def main():
     waste_level = Gauge('litterbot_waste_level', 'Level of waste', ['serial'])
     cycle_count = Gauge('litterbot_cycle_count',
                         'Number of cycles since last reset', ['serial'])
+    cycle_capacity = Gauge('litterbot_cycle_capacity',
+                        'Predicted number of cycles before full', ['serial'])
+    weight = Histogram('litterbot_weight', 'Weight of pet(s)', ['serial'])
+    night_light_level = Gauge('litterbot_night_light_level', "Brightness of night light", ['serial'])
+    ave_cycles = Gauge('litterbot_average_cycles', "Average number of cycles per day", ['serial'])
 
     while True:
         try:
@@ -56,6 +67,13 @@ async def main():
                 waste_level.labels(robot.serial).set(robot.waste_drawer_level)
                 litter_level.labels(robot.serial).set(robot.litter_level)
                 cycle_count.labels(robot.serial).set(robot.cycle_count)
+                cycle_capacity.labels(robot.serial).set(robot.cycle_capacity)
+                weight.labels(robot.serial).observe(robot.pet_weight)
+                night_light_level.labels(robot.serial).set(robot.night_light_level)
+
+                # Unpack the insights metrics
+                insights = await robot.get_insight()
+                ave_cycles.labels(robot.serial).set(insights.average_cycles)
 
         finally:
             time.sleep(600)
